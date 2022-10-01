1bc96090d11823f28113acdce57cf1a47be7d6c0
sethukumar0001 <sethukumar0001@gmail.com>
2022-10-01 16:32:33 +0530

    updated ui

--- src/pages/NotFoundPage.js
+++ src/pages/NotFoundPage.js
@@ -1,28 +1,17 @@
 import React from "react";
-import NotFound from "./lottie/not-found.json";
-import Lottie from "react-lottie";
 import { useNavigate } from "react-router-dom";
 
 function NotFoundPage(props) {
   const navigate = useNavigate();
-  const defaultOptions = {
-    loop: true,
-    autoplay: true,
-    animationData: NotFound,
-    rendererSettings: {
-      preserveAspectRatio: "xMidYMid slice",
-    },
-  };
-
   return (
     <div>
-      <Lottie
-        options={defaultOptions}
-        height={500}
-        width={500}
-        isStopped={false}
-        isPaused={false}
-      />
+      <center>
+        <img
+          src={require(`./lottie/not-found.gif`)}
+          alt="img"
+          style={{ height: "550", width: "550px" }}
+        />
+      </center>
       <h1 className="notfoundheader" onClick={() => navigate(`/`)}>
         Dashboard
       </h1>

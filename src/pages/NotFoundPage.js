b05e6c92ee30d5aea73fdad52c589d17d08387ad
sethukumar0001 <sethukumar0001@gmail.com>
2022-10-01 15:58:27 +0530

    updated ui

--- src/pages/NotFoundPage.js
+++ src/pages/NotFoundPage.js
@@ -1,11 +1,33 @@
-import React from 'react';
+import React from "react";
+import NotFound from "./lottie/not-found.json";
+import Lottie from "react-lottie";
+import { useNavigate } from "react-router-dom";
 
 function NotFoundPage(props) {
-    return (
-        <div>
-            No Page found
-        </div>
-    );
+  const navigate = useNavigate();
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: NotFound,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
+
+  return (
+    <div>
+      <Lottie
+        options={defaultOptions}
+        height={500}
+        width={500}
+        isStopped={false}
+        isPaused={false}
+      />
+      <h1 className="notfoundheader" onClick={() => navigate(`/`)}>
+        Dashboard
+      </h1>
+    </div>
+  );
 }
 
 export default NotFoundPage;

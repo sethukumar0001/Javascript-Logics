ef821340f6104c2c19177ad7fd105a34d44618c4
sethukumar0001 <sethukumar0001@gmail.com>
2022-10-04 17:31:35 +0530

    ui updated

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,12 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 function Main() {
-  function randomColor() {
-    return (
-      "#" +
-      ("00000" + ((Math.random() * 16777216) << 0).toString(16)).substr(-6)
-    );
-  }
+  // function randomColor() {
+  //   return (
+  //     "#" +
+  //     ("00000" + ((Math.random() * 16777216) << 0).toString(16)).substr(-6)
+  //   );
+  // }
 
   const [list] = useState([
     { name: "Squre", path: "/squre" },
@@ -20,7 +20,16 @@ function Main() {
           {list.map((item) => {
             return (
               <Link to={item.path}>
-                <p className="mainItem" style={{ background: randomColor() }}>
+                <p
+                  className="mainItem"
+                  style={{
+                    border: "1px solid #002140",
+                    background: "white",
+                    fontSize: "16px",
+                    fontWeight: "500px",
+                    color: "#002140",
+                  }}
+                >
                   {item.name}
                 </p>
               </Link>

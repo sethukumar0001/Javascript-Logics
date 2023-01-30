5377deee6bd4e0a5df657749cf24b825a89ee2f2
sethukumar0001 <sethukumar0001@gmail.com>
2023-01-30 20:06:02 +0530

    updated fibanocci logic

--- src/pages/JavascriptLogics/Fib-Factorial.js
+++ src/pages/JavascriptLogics/Fib-Factorial.js
@@ -15,6 +15,7 @@ function FibanocciAndFactorial(props) {
     }
     return n * FactorialFunction(n - 1);
   };
+
   return (
     <MainLayout>
       <CodeBlockComponent
@@ -22,24 +23,39 @@ function FibanocciAndFactorial(props) {
         header={"Fibanocci"}
         logicfun={() => FibanocciFunction(5)}
         logic={`
-      const fib = (n = 5) => {
-        if(n<2){
-            return n
+        //method 1
+
+        const fib = (n) => {
+          n=5;
+          if(n<2){
+              return n
+          }
+          return fib(n-1)+fib(n-2)
         }
-        return fib(n-1)+fib(n-2)
-      }
+
+        //method 2
+        const fibanocci = (n) => {
+          const fib = [0,1];
+          for (let i = 2; i < n; i++) {
+            fib[i] = fib[i-1] + fin[i-2]
+          }
+          return fib
+        }
+        console.log(fibanocci(2)) // [0,1]
+        console.log(fibanocci(3)) // [0,1,1]
+        console.log(fibanocci(5)) // [0,1,1,2,3]
           `}
       />
       <CodeBlockComponent
         header={"Factorial"}
         logicfun={() => FactorialFunction(5)}
         logic={`
-      const factorial = (n = 5) => {
-        if(n<2){
-            return 1
-        }
-        return n*factorial(n-1)
-      }
+          const factorial = (n = 5) => {
+            if(n<2){
+                return 1
+            }
+            return n*factorial(n-1)
+          }
           `}
       />
     </MainLayout>
